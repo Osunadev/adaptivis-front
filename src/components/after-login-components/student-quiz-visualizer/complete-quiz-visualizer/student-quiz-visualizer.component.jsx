2091a9d758c75c5799fb31d33d8c350788411bb9
Mirror13 <themirrorchannel1@gmail.com>
2020-05-10 15:07:53 -0700

    Added header component to student-quiz-visualizer

--- src/components/after-login-components/student-quiz-visualizer/complete-quiz-visualizer/student-quiz-visualizer.component.jsx
+++ src/components/after-login-components/student-quiz-visualizer/complete-quiz-visualizer/student-quiz-visualizer.component.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+import StudentQuizVisualizerHeader from '../quiz-visualizer-header/student-quiz-visualizer-header.component';
 
 import './quiz-styles.css';
 
@@ -6,9 +8,12 @@ const StudentQuizVisualizer = () => {
   // JSX
   return (
     <div>
+      <StudentQuizVisualizerHeader/>
+      <div>
       <h1 className='quiz-title'>Student Quiz Visualizer</h1>
       <p>Estas visualizando el quiz de motivación</p>
       <p>Párrafo nuevo</p>
+      </div>
     </div>
   );
 };
---
+++ src/components/after-login-components/student-quiz-visualizer/quiz-visualizer-header/student-quiz-visualizer-header.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './quiz-header-styles.css';
+
+const StudentQuizVisualizerHeader = () => {
+    return (
+        <div className = "quiz-header">
+            <h1>Cabecera del formulario</h1>
+        </div>
+    );
+};
+
+export default StudentQuizVisualizerHeader;
